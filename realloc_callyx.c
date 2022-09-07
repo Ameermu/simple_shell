21e4f08782c66f00ba797ce91bfc0f90290fe50d
Ameermu <ameermuhd01234@gmail.com>
2022-09-07 04:47:01 -0700

    fixed betty errors

--- realloc_callyx.c
+++ realloc_callyx.c
@@ -1,31 +1,31 @@
 #include "shell.h"
 /**
- * realloc - reallocates a new bloc of memory for a previous call by malloc
+ * _realloc - reallocates a new bloc of memory for a previous call by malloc
  * @ptr: a void pointer
- * @: an integer representing old size
+ * @a: an integer representing old size
  * @b: an integer representing the new size
  * Return: the new pointer
  */
 void *_realloc(void *ptr, int a, int b)
 {
-        if (a >= b)
-        {
-                perror("Failed to allocate memory");
+	if (a >= b)
+	{
+		perror("Failed to allocate memory");
 		exit(EXIT_FAILURE);
-        }
-        if (ptr == NULL)
-        {
-                return (NULL);
-        }
-        void *new;
-        new = malloc(b);
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        new = ptr;
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	void *new;
+
+	new = malloc(b);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new = ptr;
 
 	return (new);
 }
-
-
